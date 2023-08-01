912d2f9efaf1a15323864cb6b98dfeee0ddfb8d8
BenCampbell90 <105483919+BenCampbell90@users.noreply.github.com>
2023-08-02 00:03:56 +1000

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
       setIsLoading(true);
 
       try {
-        const response = await fetch("http://localhost:5000/api/v1/post", {
+        const response = await fetch("https://dalle-mern-13kj.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
