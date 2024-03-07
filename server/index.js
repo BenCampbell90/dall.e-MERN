51eff7705e1b31215eb598d68fb93509ec102527
Ben <bencampbell90@outlook.com>
2024-03-07 23:03:03 +1100

    cors path remove

--- server/index.js
+++ server/index.js
@@ -9,11 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: 'https://dall-e-frontend-rho.vercel.app',
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 
